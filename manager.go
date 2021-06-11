494d317f36904a6b8e261d77d9c30e70ce91b699
Goxiaoy <xy1996mail@gmail.com>
2021-06-11 15:21:46 +0800

    add config

--- manager.go
+++ manager.go
@@ -11,30 +11,39 @@ type Manager interface {
 }
 
 type manager struct {
+	cfg     *Config
 	factory DbFactory
 }
 
-func NewManager(factory DbFactory) Manager {
+type Config struct {
+	SupportNestedTransaction bool
+}
+
+func NewManager(cfg *Config, factory DbFactory) Manager {
 	return &manager{
+		cfg:     cfg,
 		factory: factory,
 	}
 }
 
 func (m *manager) WithNew(ctx context.Context, fn func(ctx context.Context) error, opt ...*sql.TxOptions) error {
-	//get current for nested
-	current, ok := FromCurrentUow(ctx)
 	factory := m.factory
-	if ok {
-		factory = func(ctx context.Context, key string) TransactionalDb {
-			//TODO lock?
-			tx, ok := current.db[key]
-			if ok {
-				tdb, ok := tx.(TransactionalDb)
+
+	//get current for nested
+	if m.cfg.SupportNestedTransaction {
+		current, ok := FromCurrentUow(ctx)
+		if ok {
+			factory = func(ctx context.Context, key string) TransactionalDb {
+				//TODO lock?
+				tx, ok := current.db[key]
 				if ok {
-					return tdb
+					tdb, ok := tx.(TransactionalDb)
+					if ok {
+						return tdb
+					}
 				}
+				return m.factory(ctx, key)
 			}
-			return m.factory(ctx, key)
 		}
 	}
 	uow := m.createNewUintOfWork(factory, opt...)
